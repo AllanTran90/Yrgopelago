151c5dd0f93a783881811a7c16b6b7092ff23c6e
AllanTran90 <allan_sweden90@hotmail.com>
2025-12-23 11:53:09 +0100

    sharped up the calender with js, still work in prossess

---
+++ assets/availability/availability.php
@@ -0,0 +1,34 @@
+<?php
+declare(strict_types=1);
+require __DIR__ . '/assets/includes/db.php';
+
+$data = $_POST['date'] ?? '' ;
+
+if (!$date){
+    echo json_encode([]);
+    exit;
+}
+
+$result = [];
+
+for ($roomId = 1; $roomId <= 3; $roomId++){
+    $sql = "
+        SELECT COUNT (*) FROM bookings
+        WHERE room_id = :room_id
+        AND arrival <= :date
+        AND departure > :date
+    ";
+ 
+
+$statement = $pdo->prepare($sql);
+$statement->execute([
+    ':room_id' => $roomId,
+    ':date' => $date
+]);
+
+    $result[$roomId] = $statement->fetchColumn() == 0;
+
+}
+
+header('Content-Type: application/json');
+echo json_encode($result);
